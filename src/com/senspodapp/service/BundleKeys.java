f41ae2faa7437ce03f894613c11097ada74f245d
Janos Gyerik <janos@axiom>
2010-10-21 09:03:16 +0900

    added bundle keys for location parameters

--- src/com/senspodapp/service/BundleKeys.java
+++ src/com/senspodapp/service/BundleKeys.java
@@ -5,5 +5,8 @@ public abstract class BundleKeys {
 	public static final String DATETIMETZ = "DTZ";
 	public static final String GPSINFO = "GPS";
 	public static final String SENSORID = "SID";
+	public static final String LOCATION_DTZ = "LDZ";
+	public static final String LOCATION_LATLON_ID = "LLL";
+	public static final String LOCATION_LOC = "LOC";
 	
 }
