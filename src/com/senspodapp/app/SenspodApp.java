a31b53f72535d38722ff2fa20a9ce7b40e11060d
shuihen@gmail.com <>
2010-10-24 15:50:36 +0900

    #94 SenspodApp  have the same menu as currently

--- src/com/senspodapp/app/SenspodApp.java
+++ src/com/senspodapp/app/SenspodApp.java
@@ -109,12 +109,24 @@ public class SenspodApp extends DeviceManagerClient {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+		case R.id.menu_connect:
+			connectSensor();
+			return true;
+		case R.id.menu_disconnect:
+			disconnectSensor();
+			break;
+		case R.id.menu_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			break;    
 		case R.id.menu_sentences:
 			startActivity(new Intent(this, SentencesActivity.class));
 			break;    
 		case R.id.menu_console:
 			startActivity(new Intent(this, DeviceManagerConsole.class));
 			break;    
+		case R.id.menu_quit:
+			finish();
+			break;
 		case R.id.menu_tabularview:
 			startActivity(new Intent(this, TabularViewActivity.class));
 			break;
