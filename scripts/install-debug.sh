e64a9e159e27e4436840dae123fc8ded15a0f57e
Janos Gyerik <janos@axiom>
2011-02-09 00:27:53 +0900

    adjusted builder script to accept a props.xml file parameter for building

--- scripts/install-debug.sh
+++ scripts/install-debug.sh
@@ -4,6 +4,8 @@ cd $(dirname "$0")/..
 
 test -f local.properties || cp samples/local.properties .
 
+test -f "$1" && grep map_server_url "$1" >/dev/null 2>/dev/null && cp "$1" res/values/props.xml
+
 ant debug
 adb install -r bin/EcoCitizen-debug.apk
 
