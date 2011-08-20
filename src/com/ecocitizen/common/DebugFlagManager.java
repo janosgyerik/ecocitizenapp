9176879f50a4c87cf76d9ebf285e5f8b99797e6a
Janos Gyerik <janos@axiom>
2011-08-20 19:26:38 +0200

    Configure DebugFlagManager using ResourceBundle and properties

--- src/com/ecocitizen/common/DebugFlagManager.java
+++ src/com/ecocitizen/common/DebugFlagManager.java
@@ -1,17 +1,26 @@
 package com.ecocitizen.common;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.ecocitizen.app.TreeViewActivity;
+import java.util.ResourceBundle;
 
 public class DebugFlagManager {
 	
-	private Map<Class<?>, Boolean> debugFlagMap;
+	private Map<String, Boolean> debugFlagMap;
 	
 	private DebugFlagManager() {
-		debugFlagMap = new HashMap<Class<?>, Boolean>();
-		debugFlagMap.put(TreeViewActivity.class, Boolean.TRUE);
+		debugFlagMap = new HashMap<String, Boolean>();
+		try {
+			ResourceBundle props = ResourceBundle.getBundle("com.ecocitizen.common.DebugFlagManager");
+			for (Enumeration<String> e = props.getKeys(); e.hasMoreElements(); ) {
+				String className = e.nextElement();
+				debugFlagMap.put(className, props.getString(className).equalsIgnoreCase("true"));
+			}
+		}
+		catch (Exception e) {
+			// Do nothing. Debug flag will be FALSE always.
+		}
 	}
 	
 	private static DebugFlagManager instance = null;
@@ -24,7 +33,7 @@ public class DebugFlagManager {
 	}
 
 	public boolean getDebugFlag(Class<?> cl) {
-		Boolean flag = debugFlagMap.get(cl);
+		Boolean flag = debugFlagMap.get(cl.getName());
 		return flag != null ? flag : false;
 	}
 
